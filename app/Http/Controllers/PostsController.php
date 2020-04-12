c05aef0021feb9e2845e1c210f5b202e30e0f03a
Yohei Kokubo <yhei.register@gmail.com>
2020-04-12 23:39:50 +0900

    記事一覧画面を作る #6

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
     public function index() {
-        return 'hello!';
+        // $posts = Post::all();
+        // $posts = Post::orderBy('created_at', 'desc')->get();
+        $posts = Post::latest()->get();
+        // $posts = [];
+        // dd($posts->toArray()); // dump die
+        // return view('posts.index', ['posts' => $posts]);
+        return view('posts.index')->with('posts', $posts);
     }
 }
