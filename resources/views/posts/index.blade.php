ceff9325dd346b860b7c304a105ed4045f9f6bb6
Yohei Kokubo <yhei.register@gmail.com>
2020-04-13 00:08:57 +0900

    コントローラとモデルを紐つける(Implicit Binding) #10

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -17,7 +17,8 @@
       --}}
       @forelse ($posts as $post)
       {{-- <li><a href="{{ url('/posts', $post->id) }}">{{ $post->title }}</a></li> --}}
-      <li><a href="{{ action('PostsController@show', $post->id) }}">{{ $post->title }}</a></li>
+      {{-- <li><a href="{{ action('PostsController@show', $post->id) }}">{{ $post->title }}</a></li> --}}
+      <li><a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a></li>
       @empty
       <li>No posts yet</li>
       @endforelse
