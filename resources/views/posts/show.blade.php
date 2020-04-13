37b3c1948facb0fd4a35fc2817d5b4710bce26ca
Yohei Kokubo <yhei.register@gmail.com>
2020-04-13 12:47:18 +0900

    共通部分を部品化していこう #12

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>{{ $post->title }}</title>
-  <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-  <div class="container">
-    <h1>{{ $post->title }}</h1>
-    <p>{!! nl2br(e($post->body)) !!}</p> <!--  !!をつけると 中身をエスケープしない  -->
-  </div>
-</body>
-</html>
+@extends('layouts.default')
+
+@section('title', $post->title)
+
+@section('content')
+<h1>{{ $post->title }}</h1>
+<p>{!! nl2br(e($post->body)) !!}</p> <!--  !!をつけると 中身をエスケープしない  -->
+@endsection
