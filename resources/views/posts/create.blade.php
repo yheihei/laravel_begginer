98aa898ec5d2783fb02bb5d6be4f915c90caad47
Yohei Kokubo <yhei.register@gmail.com>
2020-04-16 12:53:54 +0900

    バリデーションの設定をしよう #18

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -10,10 +10,17 @@
 <form action="{{ url('/posts') }}" method="post">
   {{ csrf_field() }}
   <p>
-    <input type="text" name="title" placeholder="enter title">
+    <input type="text" name="title" placeholder="enter title"
+      value="{{ old('title') }}">
+    @if ($errors->has('title'))
+    <span class="error">{{ $errors->first('title') }}</span>
+    @endif
   </p>
   <p>
-    <textarea name="body" placeholder="enter body"></textarea>
+    <textarea name="body" placeholder="enter body">{{ old('body') }}</textarea>
+    @if ($errors->has('body'))
+    <span class="error">{{ $errors->first('body') }}</span>
+    @endif
   </p>
   <p>
     <input type="submit" value="Add">
